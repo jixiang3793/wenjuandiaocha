8611c27153ad5469f996ac336c3fd049d50d4b92
guozhixiang <guozhixiang@ruijie.com.cn>
2021-03-16 14:37:04 +0800

    add timu list cache

--- apps/backend/app/controller/timu.js
+++ apps/backend/app/controller/timu.js
@@ -4,7 +4,15 @@ const Controller = require('egg').Controller;
 
 class TimuController extends Controller {
   async findAll() {
-    const result = await this.ctx.model.Timu.find();
+    let result = [];
+    if (this.app.hzcache) {
+      result = this.app.hzcache['timu'];
+    } else {
+      result = await this.ctx.model.Timu.find();
+      this.app.hzcache = {
+        'timu': result
+      };
+    }
     this.ctx.body = result;
   }
 }
